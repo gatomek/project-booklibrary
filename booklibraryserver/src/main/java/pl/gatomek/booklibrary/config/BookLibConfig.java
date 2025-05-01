11ec7a126d6a3f23b12b0664908552c7bef2f44b
Gatomek <tomasz.galinski@gmail.com>
2025-05-01 17:32:21 +0200

    server: book library folder validation added

--- booklibraryserver/src/main/java/pl/gatomek/booklibrary/config/BookLibConfig.java
+++ booklibraryserver/src/main/java/pl/gatomek/booklibrary/config/BookLibConfig.java
@@ -4,11 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+import pl.gatomek.booklibrary.validator.custom.FolderPathConstraint;
 
 @Getter
 @Setter
 @Configuration
+@Validated
 @ConfigurationProperties(prefix = "booklib")
 public class BookLibConfig {
+
+    @FolderPathConstraint
     private String folderPath;
 }
