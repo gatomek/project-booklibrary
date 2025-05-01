1ef22d441836b75010760d576474f2c3560fe7a9
Gatomek <tomasz.galinski@gmail.com>
2025-05-01 19:20:52 +0200

    REST API for downloading book added

--- booklibraryserver/src/main/java/pl/gatomek/booklibrary/rest/BookLibRestController.java
+++ booklibraryserver/src/main/java/pl/gatomek/booklibrary/rest/BookLibRestController.java
@@ -1,18 +1,21 @@
 package pl.gatomek.booklibrary.rest;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.gatomek.booklibrary.dto.LibraryItem;
 import pl.gatomek.booklibrary.service.BookLibService;
+import pl.gatomek.booklibrary.service.LibraryResource;
 
 import java.io.IOException;
 import java.io.InvalidObjectException;
+import java.nio.file.Path;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class BookLibRestController {
 
@@ -21,20 +24,24 @@ public class BookLibRestController {
 
     private final BookLibService bookLibService;
 
-    public BookLibRestController( BookLibService bookLibService) {
-        this.bookLibService = bookLibService;
+    @GetMapping(path = "/book/{hash}")
+    public ResponseEntity<ByteArrayResource> getBook(@PathVariable String hash) throws IOException {
+        LibraryResource resource = bookLibService.getBook(hash);
+        String ext = getExtension(resource.path());
+        FileExtensionToMediaType fileExt = FileExtensionToMediaType.valueOf(ext);
+
+        return ResponseEntity.ok().contentType(fileExt.getMediaType()).body(resource.resource());
     }
 
     @PostMapping(path = "/resolve", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public LibraryItem resolve( @RequestBody MultiValueMap<String, String> formData) throws IOException {
-
+    public LibraryItem resolve(@RequestBody MultiValueMap<String, String> formData) throws IOException {
         List<String> strings = formData.get(HASH);
-        if( strings != null) {
+        if (strings != null) {
             String hash = strings.getFirst();
             return bookLibService.resolve(hash);
         }
 
-        throw new InvalidObjectException( ATTR_HASH_EXPECTED);
+        throw new InvalidObjectException(ATTR_HASH_EXPECTED);
     }
 
     @GetMapping(path = "/list")
@@ -45,5 +52,15 @@ public class BookLibRestController {
     @GetMapping(path = "/reload")
     public void reload() {
         bookLibService.reload();
+    }
+
+    private String getExtension(Path path) {
+        String fileName = path.getFileName().toString();
+        int dotIndex = fileName.lastIndexOf('.');
+
+        if (dotIndex == -1 || dotIndex == fileName.length() - 1)
+            return "";
+        else
+            return fileName.substring(dotIndex + 1).toLowerCase();
     }
 }
--- booklibraryserver/src/main/java/pl/gatomek/booklibrary/service/BookLibService.java
+++ booklibraryserver/src/main/java/pl/gatomek/booklibrary/service/BookLibService.java
@@ -1,21 +1,24 @@
 package pl.gatomek.booklibrary.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import pl.gatomek.booklibrary.dto.LibraryItem;
 import pl.gatomek.booklibrary.library.Library;
 
 import java.io.FileNotFoundException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class BookLibService {
 
     private final Library library;
-
-    public BookLibService(Library library) {
-        this.library = library;
-    }
 
     public LibraryItem resolve(String hash) throws FileNotFoundException {
         return library.resolve(hash);
@@ -27,5 +30,14 @@ public class BookLibService {
 
     public void reload() {
         library.reload();
+    }
+
+    public LibraryResource getBook(String hash) throws IOException {
+        LibraryItem item = library.resolve(hash);
+
+        Path path = Paths.get(item.filePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return LibraryResource.of(path, resource);
     }
 }
