a6722f22c359738fe99af6297d10f57aadeb4b67
Gatomek <tomasz.galinski@gmail.com>
2025-05-01 14:46:19 +0200

    server configuration review

--- booklibraryserver/src/main/java/pl/gatomek/booklibrary/config/BookLibConfig.java
+++ booklibraryserver/src/main/java/pl/gatomek/booklibrary/config/BookLibConfig.java
@@ -1,15 +1,14 @@
 package pl.gatomek.booklibrary.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "booklib")
 public class BookLibConfig {
-
-    @Value("${bookLib.folderPath}")
     private String folderPath;
-
-    public String getFolderPath() {
-        return folderPath;
-    }
 }
